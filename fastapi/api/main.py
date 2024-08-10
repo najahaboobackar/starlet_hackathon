7e869e8de3aa3cbf703295423f0db5b5f5e9699c
najahaboobackar <najahaboobacker79@gmail.com>
2024-08-10 18:18:32 +0530

    deps

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+app.middleware(CORSMiddleware,
+               allow_origins=['http://localhost:3000'],
+               allow_credentials=True,
+               allow_methods=['*'],
+               allow_headers=['*']
+               )
 @app.get("/")
 def read():
     return 'hello world'
